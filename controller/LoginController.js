ca844ef55f34d1c7e3f22386cce492f1d18bb6d4
nadeeshisavindi <nadeeshisavindi@gmail.com>
2026-05-06 14:27:30 +0530

    font size

--- controller/LoginController.js
+++ controller/LoginController.js
@@ -1,21 +1,40 @@
+//
+//     $('#login_btn').on('click', function () {
+//
+//     let user = $('#username').val();
+//     let pass = $('#password').val();
+//
+//     if(user === "admin" && pass === "1234"){
+//     Swal.fire({
+//     icon: "success",
+//     title: "Login Success"
+// }).then(()=>{
+//     window.location.href = "index.html"; // go to POS
+// });
+// } else {
+//     Swal.fire({
+//     icon: "error",
+//     title: "Invalid Username or Password"
+// });
+// }
+// });
+//
 
-    $('#login_btn').on('click', function () {
+$('#login_btn').on('click', function () {
 
-    let user = $('#username').val();
-    let pass = $('#password').val();
+    let username = $('#username').val();
+    let password = $('#password').val();
 
-    if(user === "admin" && pass === "1234"){
-    Swal.fire({
-    icon: "success",
-    title: "Login Success"
-}).then(()=>{
-    window.location.href = "index.html"; // go to POS
+    // simple check (example)
+    if (username === "admin" && password === "1234") {
+
+        // store login state
+        localStorage.setItem("loggedIn", "true");
+
+        // redirect to dashboard page
+        window.location.href = "index.html";
+
+    } else {
+        Swal.fire("Error", "Invalid login", "error");
+    }
 });
-} else {
-    Swal.fire({
-    icon: "error",
-    title: "Invalid Username or Password"
-});
-}
-});
-
